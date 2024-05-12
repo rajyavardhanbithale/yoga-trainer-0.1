4cbbd50f5456c4a020c2daf6c4ffdb1a4330ca83
rajyavardhanbithale <bithale03@gmail.com>
2024-05-12 20:05:32 +0530

    type fixed

--- src/app/components/helper/MusicSelection.tsx
+++ src/app/components/helper/MusicSelection.tsx
@@ -37,7 +37,7 @@ export default function MusicSelection() {
         console.log(music);
         if(data){
             stopAudio()
-            const info:AmbientMusic = data?.filter(item => item.id === music)[0]
+            const info:any = Array.isArray(data) && data?.filter(item => item.id === music)[0]
             playNarratorAudio(info.file,'user/background','fine')   
         }
     }, [music])
@@ -55,7 +55,7 @@ export default function MusicSelection() {
             >
                 <input type="checkbox" value="" className="sr-only peer" />
                 <div
-                    onClick={() => { setSelected(!selected); handleHover(false); stopAudio();setMusic(data && data[0]?.id)}}
+                    onClick={() => { setSelected(!selected); handleHover(false); stopAudio();setMusic(data && typeof data[0] === 'object' ? data[0]?.id : undefined);}}
                     className="group peer ring-0 bg-secondary rounded-full outline-none duration-300 after:duration-300 w-[86px] h-9 shadow-md peer-checked:bg-[#00b499] peer-focus:outline-none after:content-['🍂'] after:rounded-full after:absolute after:bg-gray-50 after:outline-none after:h-7 after:w-7 after:top-[4.1px] after:left-1 after:-rotate-180 after:flex after:justify-center after:items-center peer-checked:after:translate-x-12 peer-checked:after:content-['🌿'] peer-hover:after:scale-95 peer-checked:after:rotate-0"
                 ></div>
             </label>
