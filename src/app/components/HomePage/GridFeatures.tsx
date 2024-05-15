3a63e79f47d8ae6e1475ea738e079b1dc825f691
rajyavardhanbithale <bithale03@gmail.com>
2024-05-16 00:52:01 +0530

    grid section

--- src/app/components/HomePage/GridFeatures.tsx
+++ src/app/components/HomePage/GridFeatures.tsx
@@ -1,7 +1,15 @@
-export default function GridFeatures(){
+export default function GridFeatures() {
     return (
         <>
-            GRID
+            <div className="grid grid-cols-12 mt-10 mx-10 gap-10">
+                <div className="col-span-8 h-[400px] bg-blue-400 rounded-2xl"> </div>
+                <div className="col-span-4 h-[400px] bg-blue-500 rounded-2xl"> </div>
+            </div>            
+            <div className="grid grid-cols-12 mt-5 mx-10 gap-10">
+                <div className="col-span-2 h-[200px] bg-blue-600 rounded-2xl"> </div>
+                <div className="col-span-4 h-[200px] bg-blue-700 rounded-2xl"> </div>
+                <div className="col-span-6 h-[200px] bg-blue-800 rounded-2xl"> </div>
+            </div>
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Background></Background>
       <Navbar></Navbar>
       <Hero></Hero>
-      <GridFeatures />
+      {/* <GridFeatures /> */}
 
     </>
   );
